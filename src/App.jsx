969170c072672af1e9ea027e25b23d480b2f6123
unknown <vitorfidelis091@gmail.com>
2026-06-26 14:58:43 -0300

    ajuste de imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
